8e59824a8118438e0c14a069dd38f8e6b299f205
Michele Angioni <michele.angioni@takeaway.com>
2019-11-03 19:37:29 +0100

    Completed support for Cloudevents v0.3

--- src/custom_types/cloudevents-sdk/index.ts
+++ src/custom_types/cloudevents-sdk/index.ts
@@ -1,16 +1,6 @@
 // tslint:disable max-classes-per-file
-declare module 'cloudevents-sdk' {
-  export type Spec01Payload = {
-    cloudEventsVersion: '0.1',
-    eventID: string,
-    eventType?: string,
-    source?: string,
-    eventTime?: string,
-    schemaURL?: string,
-    contentType?: string,
-    data?: any,
-  };
 
+declare module 'cloudevents-sdk' {
   export type Spec02Payload = {
     specversion: '0.2',
     id: string,
@@ -22,39 +12,18 @@ declare module 'cloudevents-sdk' {
     data?: any,
   };
 
-  export class Spec01 {
-    public payload: Spec01Payload;
-
-    public check(): void;
-
-    public type(type: string): Spec01;
-    public getType(): string|undefined;
-
-    public getSpecversion(): string;
-
-    public eventTypeVersion(eventTypeVersion: string): Spec01;
-    public getEventTypeVersion(): string|undefined;
-
-    public source(source: string): Spec01;
-    public getSource(): string|undefined;
-
-    public id(id: string): Spec01;
-    public getId(): string|undefined;
-
-    public schemaurl(schemaurl: string): Spec01;
-    public getSchemaurl(): string|undefined;
-
-    public contenttype(contenttype: string): Spec01;
-    public getContenttype(): string|undefined;
-
-    public data(data: any): Spec01;
-    public getData(): any;
-
-    public time(time: Date): Spec01;
-    public getTime(): string|undefined;
-
-    public addExtension(key: string, value: any): Spec01;
-  }
+  export type Spec03Payload = {
+    specversion: '0.3',
+    id: string,
+    type?: string,
+    source?: string,
+    time?: string,
+    schemaurl?: string,
+    datacontentencoding?: string,
+    datacontenttype?: string,
+    subject?: string,
+    data?: any,
+  };
 
   export class Spec02 {
     public payload: Spec02Payload;
@@ -90,6 +59,49 @@ declare module 'cloudevents-sdk' {
     public addExtension(key: string, value: any): Spec02;
   }
 
+  export class Spec03 {
+    public payload: Spec03Payload;
+
+    public check(): void;
+
+    public type(type: string): Spec03;
+    public getType(): string|undefined;
+
+    public getSpecversion(): string;
+
+    public eventTypeVersion(eventTypeVersion: string): Spec03;
+    public getEventTypeVersion(): string|undefined;
+
+    public source(source: string): Spec03;
+    public getSource(): string|undefined;
+
+    public id(id: string): Spec03;
+    public getId(): string|undefined;
+
+    public time(time: Date): Spec03;
+    public getTime(): string|undefined;
+
+    public schemaurl(schemaurl: string): Spec03;
+    public getSchemaurl(): string|undefined;
+
+    public dataContentEncoding(encoding: string): Spec03;
+    public getDataContentEncoding(): string|undefined;
+
+    public contenttype(contenttype: string): Spec03;
+    public getContenttype(): string|undefined;
+
+    public dataContentType(contenttype: string): Spec03;
+    public getDataContentType(): string|undefined;
+
+    public subject(contenttype: string): Spec03;
+    public getSubject(): string|undefined;
+
+    public data(data: any): Spec03;
+    public getData(): any;
+
+    public addExtension(key: string, value: any): Spec03;
+  }
+
   export type Extensions = {
     [key: string]: any,
   };
@@ -101,8 +113,8 @@ declare module 'cloudevents-sdk' {
 
   export default class Cloudevent {
     public static specs: {
-      '0.1': Spec01,
       '0.2': Spec02,
+      '0.3': Spec03,
     };
 
     public static formats: {
@@ -118,9 +130,9 @@ declare module 'cloudevents-sdk' {
       'http-binary0.2': any,
     };
 
-    public constructor(spec?: Spec01|Spec02, formatter?: JSONFormatter01);
+    public constructor(spec?: Spec02, formatter?: JSONFormatter01);
 
-    public format(): Spec01Payload|Spec02Payload;
+    public format(): Spec02Payload;
     public toString(): string;
 
     public type(type: string): Cloudevent;
@@ -149,4 +161,11 @@ declare module 'cloudevents-sdk' {
     public addExtension(key: string, value: any): Cloudevent;
     public getExtensions(): Extensions;
   }
+}
+
+declare module 'cloudevents-sdk/v03' {
+  import Cloudevent, {Spec03} from 'cloudevents-sdk';
+
+  export type Spec = Spec03;
+  export function event(): Cloudevent;
 }
--- src/events/IEventInterface.ts
+++ src/events/IEventInterface.ts
@@ -1,6 +1,6 @@
-import { Spec01Payload, Spec02Payload } from 'cloudevents-sdk';
+import {Spec02Payload, Spec03Payload} from 'cloudevents-sdk';
 
-export default interface IEventInterface<T extends Spec01Payload | Spec02Payload> {
+export default interface IEventInterface<T extends Spec02Payload | Spec03Payload> {
   getId(): string|undefined;
   getType(): string|undefined;
   getData(): any;
