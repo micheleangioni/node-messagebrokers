baac88e224bd4fb77d96d3eb705b73f734d268ab
Michele Angioni <michele.angioni@takeaway.com>
2019-12-01 18:15:47 +0100

    Exporting in IBrokerInterface from main index.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+import IBrokerInterface from './brokers/IBrokerInterface';
 import brokerFactory from './brokers/messageBroker';
 import CloudEventFactory from './events/cloudEventFactory';
 
+// Make some classes nicer to import
+export { IBrokerInterface };
 export { CloudEventFactory };
 
 export default brokerFactory;
