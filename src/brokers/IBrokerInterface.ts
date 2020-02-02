0005a944eda23a1cd943e495d7cc2205b877dcbf
Michele Angioni <michele.angioni@takeaway.com>
2020-02-02 18:04:44 +0100

    Fixed IBrokerInterface, the third argument of sendMessage() method is now optional as not all implementations make use of it

--- src/brokers/IBrokerInterface.ts
+++ src/brokers/IBrokerInterface.ts
@@ -10,6 +10,6 @@ export default interface IBrokerInterface {
   sendMessage(
     aggregate: string,
     cloudevents: IEventInterface<Cloudevent>[],
-    { partitionKey }: any,
+    { partitionKey }?: any,
   ): Promise<any>;
 }
