fdc7617c537cad287910366f8bf0387936747919
Michele Angioni <michele.angioni@takeaway.com>
2020-08-16 17:29:13 +0200

    Upgraded uuid to v8 and ts-node

--- src/events/cloudEventFactory.ts
+++ src/events/cloudEventFactory.ts
@@ -1,5 +1,5 @@
 import {CloudEvent, Version} from 'cloudevents';
-import uuidv4 from 'uuid/v4';
+import {v4 as uuidv4} from 'uuid';
 import { CreateEventV1Options } from './declarations';
 
 export default class CloudEventFactory {
