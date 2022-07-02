ba58237b71b4fe05743664b1637199203d565f79
Michele Angioni <michele.angioni@takeaway.com>
2022-07-02 19:59:01 +0200

    Fixes to kafkaJsBrokerAdapter tests

--- test/brokers/kafkaJsBrokerAdapter.spec.ts
+++ test/brokers/kafkaJsBrokerAdapter.spec.ts
@@ -1,4 +1,4 @@
-import {Consumer, KafkaMessage} from 'kafkajs';
+import {KafkaMessage} from 'kafkajs';
 import KafkaJsBrokerAdapter from '../../src/brokers/kafkaJsBrokerAdapter';
 import CloudEventFactory from '../../src/events/cloudEventFactory';
 import {KafkaJsConsumerConfig} from '../../src/brokers/declarations';
@@ -6,14 +6,6 @@ import {KafkaJsConsumerConfig} from '../../src/brokers/declarations';
 jest.setTimeout(25000); // eslint-disable-line
 
 describe('Testing the KafkaJsBrokerAdapter', () => {
-  let consumer: Consumer;
-
-  afterEach(async () => {
-    if (consumer) {
-      await consumer.disconnect();
-    }
-  });
-
   const topics = {
     user: {
       numPartitions: 1,
@@ -23,97 +15,103 @@ describe('Testing the KafkaJsBrokerAdapter', () => {
     },
   };
 
-  it('correctly creates a consumer using eachMessage and sends an event', (done) => {
-    const aggregate = 'user';
-    const eventType = 'UserCreated';
-    const data = {
-      email: 'voodoo@gmail.com',
-      username: 'Voodoo',
-    };
+  describe('with eachMessage', () => {
+    it('correctly creates a consumer using eachMessage and sends an event', (done) => {
+      const aggregate = 'user';
+      const eventType = 'UserCreated';
+      const data = {
+        email: 'voodoo@gmail.com',
+        username: 'Voodoo',
+      };
 
-    const broker = new KafkaJsBrokerAdapter(['localhost:9092'], { topics });
-    broker.init({ groupId: 'my-group' })
-      .then(() => {
-        // Init complete
+      const broker = new KafkaJsBrokerAdapter(['localhost:9092'], { topics });
 
-        const consumerConfig: KafkaJsConsumerConfig = {
-          aggregates: {
-            user: {
-              // eslint-disable-next-line @typescript-eslint/require-await
-              handler: async (message: KafkaMessage) => {
-                const eventPayload = JSON.parse(message.value?.toString() ?? '');
-                expect(eventPayload.data).toEqual(data);
-                done();
+      broker.init({ groupId: 'my-group' })
+        .then(() => {
+          // Init complete
+
+          const consumerConfig: KafkaJsConsumerConfig = {
+            aggregates: {
+              user: {
+                // eslint-disable-next-line @typescript-eslint/require-await
+                handler: async (message: KafkaMessage) => {
+                  const eventPayload = JSON.parse(message.value?.toString() ?? '');
+                  expect(eventPayload.data).toEqual(data);
+                  done();
+                },
+                topic: topics.user.topic,
               },
-              topic: topics.user.topic,
             },
-          },
-          consumerRunConfig: {
-            partitionsConsumedConcurrently: 3,
-          },
-          useBatches: false,
-        };
+            consumerRunConfig: {
+              partitionsConsumedConcurrently: 3,
+            },
+            useBatches: false,
+          };
 
-        broker.addConsumer([], consumerConfig)
-          .then((_consumer) => {
-            const cloudEvent = CloudEventFactory.createV1(
-              aggregate,
-              eventType,
-              '/users',
-              data,
-            );
+          broker.addConsumer([], consumerConfig)
+            .then((_consumer) => {
+              const cloudEvent = CloudEventFactory.createV1(
+                aggregate,
+                eventType,
+                '/users',
+                data,
+              );
 
-            setTimeout(async () => {
-              await broker.sendMessage(aggregate, [cloudEvent]);
-            }, 1000)
-          })
-      })
+              setTimeout(async () => {
+                await broker.sendMessage(aggregate, [cloudEvent]);
+              }, 1000)
+            })
+        })
+    });
   });
 
-  it('correctly creates a consumer using eachBatch and sends an event',  (done) => {
-    const aggregate = 'user';
-    const eventType = 'UserCreated';
-    const data = {
-      email: 'voodoo@gmail.com',
-      username: 'Voodoo',
-    };
+  describe('with eachBatch', () => {
+    it('correctly creates a consumer using eachBatch and sends an event',  (done) => {
+      const aggregate = 'user';
+      const eventType = 'UserCreated';
+      const data = {
+        email: 'voodoo@gmail.com',
+        username: 'Voodoo',
+      };
 
-    const broker = new KafkaJsBrokerAdapter(['localhost:9092'], { topics });
-    broker.init({ groupId: 'my-group' })
-      .then(() => {
-        // Init complete
+      const broker = new KafkaJsBrokerAdapter(['localhost:9092'], { topics });
 
-        const consumerConfig: KafkaJsConsumerConfig = {
-          aggregates: {
-            user: {
-              // eslint-disable-next-line @typescript-eslint/require-await
-              handler: async (message: KafkaMessage) => {
-                const eventPayload = JSON.parse(message.value?.toString() ?? '');
-                expect(eventPayload.data).toEqual(data);
-                done();
+      broker.init({ groupId: 'my-group' })
+        .then(() => {
+          // Init complete
+
+          const consumerConfig: KafkaJsConsumerConfig = {
+            aggregates: {
+              user: {
+                // eslint-disable-next-line @typescript-eslint/require-await
+                handler: async (message: KafkaMessage) => {
+                  const eventPayload = JSON.parse(message.value?.toString() ?? '');
+                  expect(eventPayload.data).toEqual(data);
+                  done();
+                },
+                topic: topics.user.topic,
               },
-              topic: topics.user.topic,
             },
-          },
-          consumerRunConfig: {
-            partitionsConsumedConcurrently: 3,
-          },
-          useBatches: true,
-        };
+            consumerRunConfig: {
+              partitionsConsumedConcurrently: 3,
+            },
+            useBatches: true,
+          };
 
-        broker.addConsumer([], consumerConfig)
-          .then((_consumer) => {
-            const cloudEvent = CloudEventFactory.createV1(
-              aggregate,
-              eventType,
-              '/users',
-              data,
-            );
+          broker.addConsumer([], consumerConfig)
+            .then((_consumer) => {
+              const cloudEvent = CloudEventFactory.createV1(
+                aggregate,
+                eventType,
+                '/users',
+                data,
+              );
 
-            setTimeout(async () => {
-              await broker.sendMessage(aggregate, [cloudEvent]);
-            }, 1000)
-          })
-      });
+              setTimeout(async () => {
+                await broker.sendMessage(aggregate, [cloudEvent]);
+              }, 1000)
+            })
+        });
+    });
   });
 });
